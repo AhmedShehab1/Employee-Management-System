5532e919f9ef59704e68e2bbf65e23248bfd307a
AhmedShehab1 <legendbtw2003@gmail.com>
2025-02-26 17:14:30 +0000

    test: Enhance unit tests for Department with setup and teardown methods

--- employee_management/employee_management/doctype/department/test_department.py
+++ employee_management/employee_management/doctype/department/test_department.py
@@ -1,9 +1,8 @@
 # Copyright (c) 2025, Ahmed Shehab and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests import IntegrationTestCase, UnitTestCase
-
 
 # On IntegrationTestCase, the doctype test records and all
 # link-field test record dependencies are recursively loaded
@@ -18,7 +17,30 @@ class UnitTestDepartment(UnitTestCase):
 	Use this class for testing individual functions and methods.
 	"""
 
-	pass
+	def setUp(self):
+		"""
+		Create test records before running the tests
+		"""
+		self.company = frappe.new_doc("Company")
+		self.company.name1 = "Test Company"
+		self.company.save()
+		self.department = frappe.new_doc("Department")
+		self.department.company = self.company
+		self.department.name1 = "Test Department"
+		self.department.save()
+
+	def tearDown(self):
+		"""
+		Delete test records created as part of the tests
+		"""
+		self.department.delete()
+		self.company.delete()
+
+	def test_title(self):
+		"""
+		Test the title of the department
+		"""
+		self.assertEqual(self.department.title, f"{self.department.name1} - {self.department.company}")
 
 
 class IntegrationTestDepartment(IntegrationTestCase):
